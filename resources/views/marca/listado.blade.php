d1bb4ed1b775b260c929585ab273051a02039f7f
SebastianBas <134089498+SebastianBas@users.noreply.github.com>
2023-12-01 09:58:18 -0500

    creacion de vistas

---
+++ resources/views/marca/listado.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marca</title>
+</head>
+<body>
+        
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">idMarca</th>
+            <th scope="col">Descripcion</th>
+            </tr>
+        </thead>
+        <tbody>
+</body>
