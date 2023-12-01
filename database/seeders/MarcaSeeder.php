1e546c26fadea0335d741c3cdc9f296c6ba5d3c8
SebastianBas <134089498+SebastianBas@users.noreply.github.com>
2023-12-01 08:45:40 -0500

    seeders

---
+++ database/seeders/MarcaSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MarcaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $datos = [
+            [
+                'idMarca' => '1',
+                'descripcion' => 'Nike'
+            ],
+            [
+                'idMarca' => '2',
+                'descripcion' => 'Reeebok'
+            ],
+            [
+                'idMarca' => '3',
+                'descripcion' => 'Adidas'
+            ],
+            [
+                'idMarca' => '4',
+                'descripcion' => 'Puma'
+            ],
+            
+        ];
+        BD::table('marca')->insert($datos);
+    }
+}
